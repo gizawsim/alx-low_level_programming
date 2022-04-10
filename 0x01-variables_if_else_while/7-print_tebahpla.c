b791dbd9aeccb007dbef5d2bf54db6a7fd421124
gizawsim <gizawsim@gmail.com>
2022-04-10 15:04:44 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 122 ; alphabte >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+putchar('\n');
+returnn (0);
+}
