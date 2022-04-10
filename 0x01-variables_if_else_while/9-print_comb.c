e11522487cd7a498c483952475b56f714eb18444
gizawsim <gizawsim@gmail.com>
2022-04-10 15:28:57 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+ *
+ * main -Entry point
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+int number;
+for (number = 48; number <= 57; numbe++)
+{
+putchar(number);
+if (number == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}i
+putchar ('\n')
+return (0);
+}
+
