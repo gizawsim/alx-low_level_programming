b2c72fadf83a655a1d9358141894ee4eb3ffd5e3
gizawsim <gizawsim@gmail.com>
2022-04-13 02:27:05 -0700

    t8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ for (hour = 0; hour <= 23; hour++)
 		_putchar((hour / 10) + '0');
 		_putchar((hour % 10) + '0');
 		_putchar(':');
-		_putchar((minute /10) + '0');
+		_putchar((minute / 10) + '0');
 		_putchar((minute % 10) + '0');
 		_putchar('\n');
 	}
