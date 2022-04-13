b5c05886e90aebb3eba5097ef4c8b84dcce5ef65
gizawsim <gizawsim@gmail.com>
2022-04-13 12:34:39 -0700

    T0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * main - main
+ * Return: 0
+ */
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+int main(void)
+{
+	int i;
+	i = 0;
 
-#endif
+	positive_or_negative(i)
+
+		return (0);
+}
