a372e36262f551986d219f0a5e861f84e5b71e87
gizawsim <gizawsim@gmail.com>
2022-04-14 00:00:00 -0700

    T0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * main - main
- * Return: 0
+ * main - tests function that prints if integer is positive or negative
+ * Return: zero
  */
 
 int main(void)
 {
-	int i;
-	i = 0;
+int i;
 
-	positive_or_negative(i)
+i = 0;
+positive_or_negative(i);
 
-		return (0);
+return(0);
 }
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-My Readme for the 0x03-debugging
+0x03. C - Debugging repo
