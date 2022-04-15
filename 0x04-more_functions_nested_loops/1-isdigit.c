ccf005775429df9de8df9f830bf15caba11aee71
gizawsim <gizawsim@gmail.com>
2022-04-15 02:26:04 -0700

    T1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 /**
  * _isdigit - checkes if the no is b/n 0 to 9
  * @c: input no
- * Return: 1 if is a number (0 to 9),o in other case.
+ * Return: 1 if is a number (0 to 9),0 in other case.
  */
 int _isdigit(int c)
 {
