70d7f7300391ea8bed56718ef411b90a5d33a359
gizawsim <gizawsim@gmail.com>
2022-04-18 12:00:56 -0700

    tast 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 include "main.h"
+
 /**
  * puts2 - prints every character of a string
  * @str: A pointer to an int that will be chnaged
@@ -9,7 +10,7 @@ include "main.h"
 void puts2(char *str)
 {
 	int a;
-       	for (a = 0; str[0] != '\0'; a++)
+       	for (a = 0; str[a] != '\0'; a++)
 	{
 		if (a % 2 == 0)
 			_putchar (str[a]);
