fef0d46b04e8801f291ed101f3217368c1ee6d0e
gizawsim <gizawsim@gmail.com>
2022-05-20 22:13:24 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,38 +1,30 @@
-#include "lists.h"
+#include <stdlib.h>
 #include <string.h>
+#include "lists.h"
+
 /**
- *add_node - adds a new node at the begining
- *         - of list_t list
- *@head: a ptr to the head of list_t list
- *@str:the string to be added to list_t list
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
  *
- *Return: the address of the new element, or NULL if it failed
+ * Return: the address of the new element, or NULL if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
-	list_t *nptr;
+	list_t *new;
+	unsigned int len = 0;
 
-	nptr = malloc(sizeof(list_t));
-	if (nptr == NULL)
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
-	dup = strdup(str);
-	if (dup == NULL)
-	{
-		free(nptr);
-		return (NULL);
-	}
-	for (len = 0; str[len]; len++)
-	{
-		;
-	}
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
 
-	nptr->str = dup;
-	nptr->len = len;
-	nptr->next = *head;
-
-		*head = nptr;
-	return (nptr);
+	return (*head);
 }
