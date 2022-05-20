846248a8788f50346d06bfb40f0e29a2a5e1ca5f
gizawsim <gizawsim@gmail.com>
2022-05-20 22:17:26 +0300

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,45 +1,43 @@
 #include "lists.h"
-#include <string.h>
 /**
- *add_node_end - adds a new node at the end  of a list_t list
- *@head: a ptr to the head of list_t list
- *@str: the string to be added to the list_t list
- *
- *Return: the address of the new element, or NULL if it failed
- */
+ * add_node_end - Add a new node at the end of a list.
+ * @head: Address of the first node of a list.
+ * @str: Address of the string to insert into the new node.
+ * Return: Address of the new node.
+ **/
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
-	list_t *nptr, *temp;
+	list_t *temp, *temp2;
+	unsigned int length = 0;
 
-	nptr  = malloc(sizeof(list_t));
-	if (nptr == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	dup = strdup(str);
-	if (dup == NULL)
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+
+	temp->str = strdup(str);
+	if (temp->str == NULL)
 	{
-		free(nptr);
+		free(temp);
 		return (NULL);
 	}
-	for (len = 0; str[len]; len++)
-	{
-		;
-	}
-
-	nptr->str = dup;
-	nptr->len = len;
-	nptr->next = NULL;
+	while (str[length])
+		length++;
+	temp->len = length;
+	temp->next = NULL;
 
 	if (*head == NULL)
-		*head = nptr;
-	else
 	{
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = nptr;
+		*head = temp;
+		return (temp);
 	}
-	return (*head);
+
+	temp2 = *head;
+	while (temp2->next)
+		temp2 = temp2->next;
+	temp2->next = temp;
+	return (temp);
 }
