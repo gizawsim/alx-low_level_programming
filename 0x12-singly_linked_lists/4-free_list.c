b240386d8808da30dea92c1db52e3dd208192bb4
gizawsim <gizawsim@gmail.com>
2022-05-20 22:20:09 +0300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,19 @@
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- *free_list- free's mallov'd struct and str used to create lists
- *@head: a ptr tp the list_t list
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *tmp;
 
 	while (head)
 	{
-		temp = head->next;
+		tmp = head->next;
 		free(head->str);
 		free(head);
-		head = temp;
+		head = tmp;
 	}
 }
