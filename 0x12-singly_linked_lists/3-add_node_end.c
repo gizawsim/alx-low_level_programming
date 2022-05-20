559ce6c78fa68df548e034ed2dd203b331e6a576
gizawsim <gizawsim@gmail.com>
2022-05-20 18:12:00 +0300

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,53 +1,45 @@
 #include "lists.h"
-
+#include <string.h>
 /**
- * add_node_end - adds a node at the end of the linked list
+ *add_node_end - adds a new node at the end  of a list_t list
+ *@head: a ptr to the head of list_t list
+ *@str: the string to be added to the list_t list
  *
- * @head: pointer to the first node of the list
- * @str: string to add
- *
- * Return: NULL if it fails / starting addr of the list
-*/
-
+ *Return: the address of the new element, or NULL if it failed
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *temp;
-	size_t str_len = 0;
+	char *dup;
+	int len;
+	list_t *nptr, *temp;
 
-	/*set strig length to 0 if its NULL*/
-	if (str == NULL)
-		str_len = 0;
-
-	/*count length of string*/
-	while (str[str_len] != '\0')
-		str_len++;
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	nptr  = malloc(sizeof(list_t));
+	if (nptr == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
-	new_node->len = str_len;
-	new_node->next = NULL;
-
-	/*copy our starting node pointer to temp*/
-	temp = *head;
-
-	/*if temp is NULL place our new_node at the beginning of the list*/
-	/*else if its not null we place our new_node to temp->next hence*/
-	/*adding our new_node at the end of the list*/
-	if (temp == NULL)
-		*head = new_node;
-	else
+	dup = strdup(str);
+	if (dup == NULL)
 	{
-		/**
-		 * we iterate pointer next addr if its not NULL our temp addr
-		 * points to the next addr or the next node.
-		*/
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new_node;
+		free(nptr);
+		return (NULL);
+	}
+	for (len = 0; str[len]; len++)
+	{
+		;
 	}
 
+	nptr->str = dup;
+	nptr->len = len;
+	nptr->next = NULL;
+
+	if (*head == NULL)
+		*head = nptr;
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = nptr;
+	}
 	return (*head);
 }
