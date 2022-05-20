c50dda2e85c00aa6ebbc9fd89e64e4ede4a43388
gizawsim <gizawsim@gmail.com>
2022-05-20 18:06:34 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,38 +1,46 @@
 #include "lists.h"
-#include <string.h>
 
 /**
- * *add_node - adds a node to a linked list head
- * @head: linked list head
- * @str: string element of the linked list
- * Return: number of elements
- */
+ * add_node - adds a node at the beginning of the linked list
+ *
+ * @head: pointer to the first node of the list
+ * @str: string to add to new node in the list
+ *
+ * Return: NULL if it fails / starting addr of the list
+*/
 
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	int len_str;
+	size_t str_len = 0;
 
-	new_node = (list_t *) malloc(sizeof(list_t));
+	/*set strig length to 0 if its NULL*/
+	if (str == NULL)
+		str_len = 0;
 
+	/*count length of string*/
+	while (str[str_len] != '\0')
+		str_len++;
+
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
-		free(new_node);
 		return (NULL);
-	}
+
+	/**
+	 * initialize the next addr of new_node
+	 * to NULL if head is NULL, if it's not
+	 * move the first node to the addr of new_node
+	 * ->next hence adding new_node at the beginning
+	 *  of the list
+	*/
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
 
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	for (len_str = 0; str[len_str] != '\0';  len_str++)
-	;
-
-	new_node->len = len_str;
-	new_node->next = *head;
+	new_node->len = str_len;
 	*head = new_node;
 
-	return (new_node);
+	return (*head);
 }
