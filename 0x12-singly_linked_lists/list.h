206e0332f33c0f36f53241a210c47bea4c71d059
gizawsim <gizawsim@gmail.com>
2022-05-20 18:12:41 +0300

    Update list.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,10 +1,6 @@
-#ifndef LIST_H
-#define LIST_H
-
-#include <stdio.h>
-#include <string.h>
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -13,7 +9,7 @@
  *
  * Description: singly linked list node structure
  * for Holberton project
-*/
+ */
 typedef struct list_s
 {
 	char *str;
@@ -21,11 +17,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* LIST_H */
+#endif
