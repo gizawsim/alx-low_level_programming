6a4cb6e82399d3c18dac10e33c211d3d1b943ba0
gizawsim <gizawsim@gmail.com>
2022-05-26 21:11:42 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,7 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stddef.h>
-#include <stdio.h>
-
-#ifdef DEBUG /*prints debug statements*/
-#endif
-
+/*prototypes*/
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
