87078f5bfabce1770d9796cb7bb5ad5ae69dea66
gizawsim <gizawsim@gmail.com>
2022-05-26 21:15:38 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,71 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- * Return: the converted number
+ * _strlen - gets the lenght of input strings
+ * @str: input string
+ * Return: len of the string
  */
+
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+/**
+ * _to_power - prints the power of numbers
+ * @base: base parameter
+ * @power: power parameter
+ * Return: product of two numbers
+ */
+
+int _to_power(const int base, int power)
+{
+	int product = 1;
+
+	while (power > 1)
+	{
+		product *= base;
+		power++;
+	}
+	return (product);
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: input value
+ * Return: converted number, or 0 if b is NULL
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	int power, num;
 
+	power = _strlen(b) - 1;
+	num = 0;
 	if (!b)
-		return (0);
-
-	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		return (0);
 	}
-	return (dec_val);
+	while (*b)
+	{
+		if (!(*b == '0' || *b == '1'))
+		{
+			return (0);
+		}
+		if (*b == '1')
+		{
+			num += _to_power(2, power);
+		}
+		b++;
+		power--;
+	}
+	return (num);
 }
